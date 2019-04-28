e034a90ab67fe81f3e781678d2952345347bc25e
Jesse Creech <jesse.creech@gmail.com>
2019-04-28 18:25:04 +1000

    save files

--- 100/100/Program.cs
+++ 100/100/Program.cs
@@ -8,6 +8,34 @@ namespace _100 {
 
         }
 
+        private int MovesToNext (Tuple<int, int> position, Tuple<int, int> destination) {
+            // figure out which way im moving
+            positionX = position.Item1;
+            positionY = position.Item2;
+
+            destinationX = destination.Item1;
+            destinationY = destination.Item2;
+
+            //moving left
+            if (positionX > destinationX) {
+
+            }
+            //moving down
+            if (positionY > destinationY) {
+
+            }
+            // moving right
+            if (positionX < destinationX) {
+
+            }
+            // Moving up
+            if (positionY > destinationY) {
+
+            }
+
+            //
+        }
+
         static void Main (string[] args) {
             Console.WriteLine ("Hello World!");
         }
