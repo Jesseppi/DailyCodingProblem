dbecb0c8f655c3a72d5acf0c008c8c38c46d0eb7
Jesse <jessec@Jesses-MBP.home.gateway>
2019-05-11 12:11:45 +1000

    add #165, and complete

---
+++ 165/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _165
+{
+    class Program
+    {
+        public static int[] GetManyNumbersAreBiggerOnTheRight(int[] numbers){
+            var arrayLength = numbers.Length;
+
+            var biggerNumbersArray = new int[arrayLength];
+
+            var tallyDictionary = new Dictionary<int,int>();
+
+            for(var index = 0; index < arrayLength; index++){
+                tallyDictionary.Add(index,0);
+                var indexValue = numbers[index];
+                for(var compareIndex = 0; compareIndex < index; compareIndex ++)
+                {
+                    var compareValue = numbers[compareIndex];
+                    if(compareValue - indexValue > 0) tallyDictionary[compareIndex]++;
+                }
+            }
+
+            tallyDictionary.Values.CopyTo(biggerNumbersArray,0);
+            return biggerNumbersArray;
+        }
+        static void Main(string[] args)
+        {
+            var biggerArray = GetManyNumbersAreBiggerOnTheRight(new int[] { 3, 4, 9, 6, 1 });
+            var retrunString = "[";
+            for(var item = 0; item < biggerArray.Length; item++)
+            {
+                retrunString = retrunString + biggerArray[item];
+                if(item != biggerArray.Length-1) retrunString = retrunString + ",";
+            }
+            retrunString = retrunString + "]";
+            Console.WriteLine(retrunString);
+
+        }
+    }
+}
